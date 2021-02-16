d2ff423087220bf4dcead50ffa84f6658f3fbd1d
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2021-02-16 13:18:05 +0700

    16:02:2021

---
+++ OClab2/cmake-build-debug/CMakeFiles/OClab2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OClab2.dir/main.c.obj"
+  "OClab2.exe"
+  "OClab2.exe.manifest"
+  "OClab2.pdb"
+  "libOClab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OClab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
