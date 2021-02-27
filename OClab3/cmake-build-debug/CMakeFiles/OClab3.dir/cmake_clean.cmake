8bbc580abef86d035ebdab94552753010e2a6a07
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2021-02-27 22:44:12 +0700

    27.02.2021 22:44

---
+++ OClab3/cmake-build-debug/CMakeFiles/OClab3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OClab3.dir/main.c.obj"
+  "OClab3.exe"
+  "OClab3.exe.manifest"
+  "OClab3.pdb"
+  "libOClab3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OClab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
