e87c38fc291f6c1bae176627f86906e191b12314
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2021-04-12 22:02:54 +0700

    12.04.2021

--- OClab5/main.c
+++ OClab5/main.c
@@ -3,11 +3,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <stdbool.h>
 #include <string.h>
 
 #define BUF_SIZE 15
-
+#define OPEN_FILE_FAIL 1
+#define FILL_TABLE_FAIL 2
+#define PRINT_LINE_FAIL 3
 
 typedef struct t_e{
     int offset;
@@ -37,6 +39,12 @@ table* init_table(){
     return T;
 }
 
+void free_table(table* T){
+    for(int i = 0; i < T->curr_len; i++){
+        free(&T->array[i]);
+    }
+    free(T);
+}
 void add_elem_to_table(table* T,table_elem* T_elem){
     if(T->curr_len+1 > T->arr_len){
         T->arr_len = T->arr_len*2;
@@ -51,58 +59,17 @@ void add_elem_to_table(table* T,table_elem* T_elem){
     T->curr_len++;
 }
 
-void print_table(table* T){
-    for(int i = 0; i < T->curr_len; i++){
-        printf("%d offset: %d strlen: %d\n",i+1, T->array[i].offset, T->array[i].len);
-    }
-}
-
-void print_file(table* T, int fd){
-    print_table(T);
-    for(int i = 0; i < T->curr_len; i++){
-        if(lseek(fd, T->array[i].offset, 0) == -1L){
-            perror("Seek error");
-            exit(2);
-        }
-        char *buf = malloc(sizeof (char)*T->array[i].len);
-        read(fd, buf, T->array[i].len);
-        for(int j = 0; j < T->array[i].len;j++){
-            printf("%c",buf[j]);
-        }
-        printf("\n");
-        free(buf);
-    }
-}
-
-
-int main() {
-
-    char filename[BUF_SIZE];
-    scanf("%s", filename);
-    //printf("%s", filename);
-    //printf("%d", (int)strlen(filename));
-
-    int fd;
-    if((fd = open(filename,O_RDONLY)) == -1){
-        perror("can't open file");
-        exit(1);
-    }
-
+bool fill_table(table* T, int fd){
     char *buffer = malloc(sizeof (char)*BUF_SIZE);
-
     int offset = 0;
     if(lseek(fd,offset,0) == -1L){
         perror("Seek error");
-        exit(2);
+        return false;
     }
-
     if(read(fd, buffer, BUF_SIZE) == 0){
         printf("empty file\n");
-        return 0;
+        return false;
     }
-
-
-    table* my_table = init_table();
     char printing_symbol;
     int tmp_offset = 0;
     int tmp_len = 0;
@@ -110,11 +77,11 @@ int main() {
     while(i < strlen(buffer)){
         printing_symbol = buffer[i];
         if(printing_symbol == EOF){
-            add_elem_to_table(my_table, make_table_elem(tmp_offset,tmp_len));
+            add_elem_to_table(T, make_table_elem(tmp_offset,tmp_len));
             break;
         }
         if(printing_symbol == '\n'){
-            add_elem_to_table(my_table, make_table_elem(tmp_offset,tmp_len));
+            add_elem_to_table(T, make_table_elem(tmp_offset,tmp_len));
             tmp_offset += tmp_len+1;
             tmp_len = 0;
         }else{
@@ -126,7 +93,7 @@ int main() {
             offset += strlen(buffer);
             if(lseek(fd,offset,0) == -1L){
                 perror("Seek error");
-                exit(2);
+                return false;
             }
 
             if(read(fd, buffer, BUF_SIZE) == 0){
@@ -135,12 +102,87 @@ int main() {
             i = 0;
         }
     }
-    //print_table(my_table);
-
-    print_file(my_table,fd);
-
+    return true;
+}
 
 
+void print_table(table* T){
+    for(int i = 0; i < T->curr_len; i++){
+        printf("%d offset: %d strlen: %d\n",i+1, T->array[i].offset, T->array[i].len);
+    }
+}
 
+bool print_numbered_line(table* T, int fd){
+    int number;
+    scanf("%d", &number);
+    while(number > T->curr_len || number < 0){
+        printf("unavailable line number, please enter another number\n");
+        scanf("%d", &number);
+    }
+    while (number != 0) {
+        number--;
+        if (lseek(fd, T->array[number].offset, 0) == -1L) {
+            perror("Seek error");
+            return false;
+        }
+        char *buf = malloc(sizeof(char) * T->array[number].len);
+        read(fd, buf, T->array[number].len);
+        for (int j = 0; j < T->array[number].len; j++) {
+            printf("%c", buf[j]);
+        }
+        printf("\n");
+        free(buf);
+        scanf("%d", &number);
+        while(number > T->curr_len || number < 0){
+            printf("unavailable line number, please enter another number\n");
+            scanf("%d", &number);
+        }
+    }
+    return true;
+}
+
+bool print_file(table* T, int fd){
+    print_table(T);
+    for(int i = 0; i < T->curr_len; i++){
+        if(lseek(fd, T->array[i].offset, 0) == -1L){
+            perror("Seek error");
+            return false;
+        }
+        char *buf = malloc(sizeof (char)*T->array[i].len);
+        read(fd, buf, T->array[i].len);
+        for(int j = 0; j < T->array[i].len;j++){
+            printf("%c",buf[j]);
+        }
+        printf("\n");
+        free(buf);
+    }
+    return true;
+}
+
+
+int main() {
+
+    char filename[BUF_SIZE];
+    scanf("%s", filename);
+
+    int fd;
+    int fd_checker = (fd = open(filename,O_RDONLY));
+    if(fd_checker == -1){
+        perror("can't open file");
+        exit(OPEN_FILE_FAIL);
+    }
+
+    table* my_table = init_table();
+    bool table_successfully_filled = fill_table(my_table, fd);
+    if(!table_successfully_filled){
+        exit(FILL_TABLE_FAIL);
+    }
+
+    bool lines_successfully_printed = print_numbered_line(my_table,fd);
+    if(!lines_successfully_printed){
+        exit(PRINT_LINE_FAIL);
+    }
+
+    free_table(my_table);
     return 0;
 }
